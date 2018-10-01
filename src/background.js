b04b1b17f57bc562004ae747bedda3d7b5805437
wojciech.sowa <wojciech.sowa@makingwaves.com>
2018-10-01 12:00:25 +0200

    fix issue with not existing links until menu is clicked

--- src/background.js
+++ src/background.js
@@ -19,19 +19,26 @@
 chrome.pageAction.onClicked.addListener(function(activeTab) {
 
     chrome.storage.sync.get("data", function(items) {
-        if (!chrome.runtime.error) {
-            var languages = items.data.split(";");
+        if (items && items.data) {
 
-            for (var i = 0; i < languages.length; i++) {
-                var x = 'document.getElementsByClassName("language-item locale code_' + languages[i] + '")[0].getElementsByTagName("span")[0].attributes[0].value';
-                chrome.tabs.executeScript(activeTab.id, { code: x }, function(result) {
-                    chrome.tabs.create({ url: result[0] }, function(newTab) {
-                        console.log(newTab);
+            var menu = 'document.querySelector("[data-header-btn=' + '\'menu\'' + ']").click();';
+            chrome.tabs.executeScript(activeTab.id, { code: menu }, function() {}); //toggle menu
+            chrome.tabs.executeScript(activeTab.id, { code: menu }, function() {}); //close menu
+
+            if (!chrome.runtime.error) {
+                var languages = items.data.split(";");
+
+                for (var i = 0; i < languages.length; i++) {
+                    var x = 'document.getElementsByClassName("language-item locale code_' + languages[i] + '")[0].getElementsByTagName("span")[0].attributes[0].value';
+                    chrome.tabs.executeScript(activeTab.id, { code: x }, function(result) {
+                        chrome.tabs.create({ url: result[0] }, function(newTab) {
+                            console.log(newTab);
+                        });
                     });
-                });
+                }
+            } else {
+                console.log("Something went wrong.");
             }
-        } else {
-            console.log("Something went wrong.");
-        }
+        };
     });
 });
