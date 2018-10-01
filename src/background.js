36ef16c673dac24841628b03fb602bac23661dd5
wojciech.sowa <wojciech.sowa@makingwaves.com>
2018-10-01 11:22:24 +0200

    Initial commit with working extension

---
+++ src/background.js
@@ -0,0 +1,37 @@
+﻿chrome.runtime.onInstalled.addListener(function() {
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+
+
+        chrome.declarativeContent.onPageChanged.addRules([{
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { hostContains: 'visitnorway' }
+                    })
+                ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+
+        ]);
+    });
+});
+
+chrome.pageAction.onClicked.addListener(function(activeTab) {
+
+    chrome.storage.sync.get("data", function(items) {
+        if (!chrome.runtime.error) {
+            var languages = items.data.split(";");
+
+            for (var i = 0; i < languages.length; i++) {
+                var x = 'document.getElementsByClassName("language-item locale code_' + languages[i] + '")[0].getElementsByTagName("span")[0].attributes[0].value';
+                chrome.tabs.executeScript(activeTab.id, { code: x }, function(result) {
+                    chrome.tabs.create({ url: result[0] }, function(newTab) {
+                        console.log(newTab);
+                    });
+                });
+            }
+        } else {
+            console.log("Something went wrong.");
+        }
+    });
+});
---
+++ src/options.js
@@ -0,0 +1,39 @@
+﻿document.body.onload = function() {  
+  chrome.storage.sync.get("data", function(items) {
+    if (!chrome.runtime.error) {
+	  var languages = items.data.split(";");
+	  
+	  for(var i=0;i<languages.length;i++){
+				document.getElementById(languages[i]).checked = true;
+	  }
+     }
+	 else {
+		console.log("Something went wrong.");
+	 }
+	 
+	});
+  
+}
+
+document.getElementById("set").onclick = function() {
+  
+  var checkedBoxes = document.querySelectorAll('input[name=languages]:checked');
+  
+  var selectedLanguages ="";
+  
+  for(var i=0;i<checkedBoxes.length;i++)
+  {
+	  selectedLanguages += checkedBoxes[i].id + ";";
+  }
+  
+  selectedLanguages = selectedLanguages.slice(0, -1);
+  
+  chrome.storage.sync.set({ "data" : selectedLanguages }, function() {
+    if (chrome.runtime.error) {
+      alert("Something went wrong");
+    }
+	alert("Saved successfully");
+  });
+  
+  window.close();
+}
