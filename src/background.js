6967ab13cf1db97d511515f2777675d9c599b316
lukasz.szumilas <lukasz.szumilas@makingwaves.com>
2018-10-26 10:38:55 +0200

    VisitNorway chrome extension updates
    - added select all / none buttons in options
    - added ability to switch languages listings
    - fixed runtime errors and formatted code

--- src/background.js
+++ src/background.js
@@ -1,7 +1,5 @@
-﻿chrome.runtime.onInstalled.addListener(function() {
-
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        console.log("contains " + tab.url.includes('visitnorway'));
+﻿chrome.runtime.onInstalled.addListener(function () {
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         if (tab.url.includes('visitnorway')) {
             chrome.pageAction.show(tabId);
         } else {
@@ -10,49 +8,52 @@
     });
 });
 
-chrome.pageAction.onClicked.addListener(function(activeTab) {
+chrome.pageAction.onClicked.addListener(function (activeTab) {
+    chrome.storage.local.get(['data'], function (items) {
+        if (items && items.data && items.data != '') {
+            var menu = 'document.querySelector("[data-header-btn=\'menu\']").click();';
 
-    chrome.storage.local.get("data", function(items) {
-        if (items && items.data) {
-
-            var menu = 'document.querySelector("[data-header-btn=' + '\'menu\'' + ']").click();';
-
-            chrome.tabs.executeScript(activeTab.id, { code: menu }, function() {}); //toggle menu
-            setTimeout(function() {}, 500);
-            chrome.tabs.executeScript(activeTab.id, { code: menu }, function() {}); //close menu
-            setTimeout(function() {}, 500);
+            chrome.tabs.executeScript(activeTab.id, { code: menu }, function () { }); //toggle menu
+            chrome.tabs.executeScript(activeTab.id, { code: menu }, function () { }); //close menu
 
             if (!chrome.runtime.error) {
                 var languages = items.data.split(";");
-                var postfix = [];
-                for (var i = 0; i < languages.length; i++) {
-                    var x = 'document.querySelectorAll("[data-language-site=' + languages[i] 
-                    + ']")[0].getElementsByTagName("span")[0].attributes[0].value';
 
-                    if(languages[i] == 'uk' || languages[i] == 'usa'){
+                for (let i = 0; i < languages.length; i++) {
+                    var x = 'document.querySelectorAll("[data-language-site=' + languages[i] + ']")[0].getElementsByTagName("a")[0].href';
+                    let postfix = [];
+                    if (languages[i] == 'uk' || languages[i] == 'usa' || languages[i] == 'primary') {
                         postfix.push('?lang=' + languages[i]);
-                    }                    
+                    }
 
-                    chrome.tabs.executeScript(activeTab.id, { code: x }, function(result) {
+                    chrome.tabs.executeScript(activeTab.id, { code: x }, function (result) {
                         var url = result[0];
-
-                        if(url.includes('.com/') && postfix.length > 0){
-                            url += postfix[0];
-                            postfix = postfix.splice(1,1); 
+                        if (url == '') {
+                            // in case of listing page there are no links inside html
+                            // opens same link in new tab and then clicks on specific language to load it
+                            // delays are necessary to let DOM render everything and attach all events
+                            chrome.tabs.create({ url: activeTab.url }, function (newTab) {
+                                let x = "setTimeout(function(){document.querySelectorAll('li.language-item.locale.code_" + languages[i] + " a')[0].click();},3000);";
+                                setTimeout(function () {
+                                    chrome.tabs.executeScript(newTab.id, { code: x }, function (r) { });
+                                }, 2000);
+                                console.log(newTab);
+                            });
+                        } else {
+                            if (url.includes('.com/') && postfix.length > 0) {
+                                url += postfix[0];
+                            }
+                            chrome.tabs.create({ url: url }, function (newTab) {
+                                console.log(newTab);
+                            });
                         }
-
-                        chrome.tabs.create({ url: url}, function(newTab) {
-                            console.log(newTab);
-                        });
                     });
                 }
             } else {
-                console.log("Something went wrong.");
+                console.log('Something went wrong.');
             }
         } else {
-            //open options
             chrome.runtime.openOptionsPage();
         }
     });
 });
-
--- src/options.js
+++ src/options.js
@@ -1,39 +1,40 @@
-﻿document.body.onload = function() {  
-  chrome.storage.local.get("data", function(items) {
-    if (!chrome.runtime.error) {
-	  var languages = items.data.split(";");
-	  
-	  for(var i=0;i<languages.length;i++){
-				document.getElementById(languages[i]).checked = true;
-	  }
-     }
-	 else {
-		console.log("Something went wrong.");
-	 }
-	 
-	});
-  
-}
+﻿document.body.onload = function () {
+    chrome.storage.local.get("data", function (items) {
+        if (!chrome.runtime.error && items != '' && items.data != undefined && items.data != '') {
+            var languages = items.data.split(";");
+            for (var i = 0; i < languages.length; i++) {
+                document.getElementById(languages[i]).checked = true;
+            }
+        }
+    });
+};
 
-document.getElementById("set").onclick = function() {
-  
-  var checkedBoxes = document.querySelectorAll('input[name=languages]:checked');
-  
-  var selectedLanguages ="";
-  
-  for(var i=0;i<checkedBoxes.length;i++)
-  {
-	  selectedLanguages += checkedBoxes[i].id + ";";
-  }
-  
-  selectedLanguages = selectedLanguages.slice(0, -1);
-  
-  chrome.storage.local.set({ "data" : selectedLanguages }, function() {    
-    if (chrome.runtime.error) {
-      alert("Something went wrong");
+document.getElementById("set").onclick = function () {
+    var checkedBoxes = document.querySelectorAll('input[name=languages]:checked');
+    var selectedLanguages = [];
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        selectedLanguages.push(checkedBoxes[i].id);
     }
-	alert("Saved successfully");
-  });
-  
-  window.close();
-}
+
+    chrome.storage.local.set({ "data": selectedLanguages.join(';') }, function () {
+        if (chrome.runtime.error) {
+            alert("Something went wrong");
+        }
+        alert("Saved successfully");
+    });
+    window.close();
+};
+
+document.getElementById("select-all").onclick = function () {
+    var checkedBoxes = document.querySelectorAll('input[name=languages]');
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        checkedBoxes[i].checked = true;
+    }
+};
+
+document.getElementById("select-none").onclick = function () {
+    var checkedBoxes = document.querySelectorAll('input[name=languages]');
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        checkedBoxes[i].checked = false;
+    }
+};
